f2f9d21f265a0a5093698204109027cc9f082be3
Red-exe-Engineer <90420989+Red-exe-Engineer@users.noreply.github.com>
2023-09-04 01:01:36 +0000

    Redo entity API (part 1)

--- mcpi_addons/entity.py
+++ mcpi_addons/entity.py
@@ -1,9 +1,21 @@
-class Entity:
-    """Minecraft PI entity description. Can be sent to Minecraft.spawnEntity"""
+"""
+    Modified entity.py - Wallee/Red-exe-Engineer
+    This modified library removes unused entity constants and fixes broken ones.
+    It also adds a Sheep and Tile class as well:
+        Sheep is used for summoning dyed sheep entities.
+        Tile is dynamically created based on block.py and is used for summoning tile drop entities.
+"""
 
-    def __init__(self, id, name=None):
+from . import block
+
+
+class Entity:
+    """ Minecraft PI entity description. Can be sent to Minecraft.entity.spawn """
+
+    def __init__(self, id, name, data=0):
         self.id = id
         self.name = name
+        self.data = data
 
     def __cmp__(self, rhs):
         return hash(self) - hash(rhs)
@@ -15,89 +27,78 @@ class Entity:
         return self.id
 
     def __iter__(self):
-        """Allows an Entity to be sent whenever id is needed"""
+        """ Allows an Entity to be sent whenever id is needed """
+
         return iter((self.id,))
 
     def __repr__(self):
-        return "Entity(%d)" % (self.id)
+        return f'Entity({self.name} {self.id})'
 
 
-EXPERIENCE_ORB = Entity(2, "EXPERIENCE_ORB")
-AREA_EFFECT_CLOUD = Entity(3, "AREA_EFFECT_CLOUD")
-ELDER_GUARDIAN = Entity(4, "ELDER_GUARDIAN")
-WITHER_SKELETON = Entity(5, "WITHER_SKELETON")
-STRAY = Entity(6, "STRAY")
-EGG = Entity(7, "EGG")
-LEASH_HITCH = Entity(8, "LEASH_HITCH")
-PAINTING = Entity(9, "PAINTING")
-ARROW = Entity(10, "ARROW")
-SNOWBALL = Entity(11, "SNOWBALL")
-FIREBALL = Entity(12, "FIREBALL")
-SMALL_FIREBALL = Entity(13, "SMALL_FIREBALL")
-ENDER_PEARL = Entity(14, "ENDER_PEARL")
-ENDER_SIGNAL = Entity(15, "ENDER_SIGNAL")
-THROWN_EXP_BOTTLE = Entity(17, "THROWN_EXP_BOTTLE")
-ITEM_FRAME = Entity(18, "ITEM_FRAME")
-WITHER_SKULL = Entity(19, "WITHER_SKULL")
-PRIMED_TNT = Entity(20, "PRIMED_TNT")
-HUSK = Entity(23, "HUSK")
-SPECTRAL_ARROW = Entity(24, "SPECTRAL_ARROW")
-SHULKER_BULLET = Entity(25, "SHULKER_BULLET")
-DRAGON_FIREBALL = Entity(26, "DRAGON_FIREBALL")
-ZOMBIE_VILLAGER = Entity(27, "ZOMBIE_VILLAGER")
-SKELETON_HORSE = Entity(28, "SKELETON_HORSE")
-ZOMBIE_HORSE = Entity(29, "ZOMBIE_HORSE")
-ARMOR_STAND = Entity(30, "ARMOR_STAND")
-DONKEY = Entity(31, "DONKEY")
-MULE = Entity(32, "MULE")
-EVOKER_FANGS = Entity(33, "EVOKER_FANGS")
-EVOKER = Entity(34, "EVOKER")
-VEX = Entity(35, "VEX")
-VINDICATOR = Entity(36, "VINDICATOR")
-ILLUSIONER = Entity(37, "ILLUSIONER")
-MINECART_COMMAND = Entity(40, "MINECART_COMMAND")
-BOAT = Entity(41, "BOAT")
-MINECART = Entity(42, "MINECART")
-MINECART_CHEST = Entity(43, "MINECART_CHEST")
-MINECART_FURNACE = Entity(44, "MINECART_FURNACE")
-MINECART_TNT = Entity(45, "MINECART_TNT")
-MINECART_HOPPER = Entity(46, "MINECART_HOPPER")
-MINECART_MOB_SPAWNER = Entity(47, "MINECART_MOB_SPAWNER")
-CREEPER = Entity(50, "CREEPER")
-SKELETON = Entity(51, "SKELETON")
-SPIDER = Entity(52, "SPIDER")
-GIANT = Entity(53, "GIANT")
-ZOMBIE = Entity(54, "ZOMBIE")
-SLIME = Entity(55, "SLIME")
-GHAST = Entity(56, "GHAST")
-PIG_ZOMBIE = Entity(57, "PIG_ZOMBIE")
-ENDERMAN = Entity(58, "ENDERMAN")
-CAVE_SPIDER = Entity(59, "CAVE_SPIDER")
-SILVERFISH = Entity(60, "SILVERFISH")
-BLAZE = Entity(61, "BLAZE")
-MAGMA_CUBE = Entity(62, "MAGMA_CUBE")
-ENDER_DRAGON = Entity(63, "ENDER_DRAGON")
-WITHER = Entity(64, "WITHER")
-BAT = Entity(65, "BAT")
-WITCH = Entity(66, "WITCH")
-ENDERMITE = Entity(67, "ENDERMITE")
-GUARDIAN = Entity(68, "GUARDIAN")
-SHULKER = Entity(69, "SHULKER")
-PIG = Entity(90, "PIG")
-SHEEP = Entity(91, "SHEEP")
-COW = Entity(92, "COW")
-CHICKEN = Entity(93, "CHICKEN")
-SQUID = Entity(94, "SQUID")
-WOLF = Entity(95, "WOLF")
-MUSHROOM_COW = Entity(96, "MUSHROOM_COW")
-SNOWMAN = Entity(97, "SNOWMAN")
-OCELOT = Entity(98, "OCELOT")
-IRON_GOLEM = Entity(99, "IRON_GOLEM")
-HORSE = Entity(100, "HORSE")
-RABBIT = Entity(101, "RABBIT")
-POLAR_BEAR = Entity(102, "POLAR_BEAR")
-LLAMA = Entity(103, "LLAMA")
-LLAMA_SPIT = Entity(104, "LLAMA_SPIT")
-PARROT = Entity(105, "PARROT")
-VILLAGER = Entity(120, "VILLAGER")
-ENDER_CRYSTAL = Entity(200, "ENDER_CRYSTAL")
+class Sheep(Entity):
+    """ Minecraft PI sheep description. Can be send to Minecraft.entity.spawn """
+
+    def __init__(self, data, color):
+        super().__init__(13, color, data)
+
+    def __repr__(self):
+        return f'Sheep({self.name}, {self.id}, {self.data})'
+
+
+class Item(Entity):
+    def __init__(self, data, name):
+        print(name, data)
+        super().__init__(64, name, data)
+
+    def __repr__(self):
+        return f'Item({self.name}, {self.id})'
+
+
+# Passive
+CHICKEN          = Entity(10, "CHICKEN")
+COW              = Entity(11, "COW")
+PIG              = Entity(12, "PIG")
+
+# Sheep
+WHITE_SHEEP      = Sheep(0, "WHITE")
+ORANGE_SHEEP     = Sheep(1, "ORANGE")
+MAGENTA_SHEEP    = Sheep(2, "MAGENTA")
+LIGHT_BLUE_SHEEP = Sheep(3, "LIGHT_BLUE")
+YELLOW_SHEEP     = Sheep(4, "YELLOW")
+LIME_SHEEP       = Sheep(5, "LIME")
+PINK_SHEEP       = Sheep(6, "PINK")
+GRAY_SHEEP       = Sheep(7, "GRAY")
+LIGHT_GRAY_SHEEP = Sheep(8, "LIGHT_GRAY")
+CYAN_SHEEP       = Sheep(9, "CYAN")
+PURPLE_SHEEP     = Sheep(10, "PURPLE")
+BLUE_SHEEP       = Sheep(11, "BLUE")
+BROWN_SHEEP      = Sheep(12, "BROWN")
+GREEN_SHEEP      = Sheep(13, "GREEN")
+RED_SHEEP        = Sheep(14, "RED")
+BLACK_SHEEP      = Sheep(15, "BLACK")
+
+SHEEP = WHITE_SHEEP
+
+# Hostile
+ZOMBIE           = Entity(32, "ZOMBIE")
+CREEPER          = Entity(33, "CREEPER")
+SKELETON         = Entity(34, "SKELETON")
+SPIDER           = Entity(35, "SPIDER")
+PIG_ZOMBIE       = Entity(36, "PIG_ZOMBIE")
+
+# Tiles
+TNT              = Entity(65, "TNT")
+ARROW            = Entity(80, "ARROW")
+SNOWBALL         = Entity(81, "SNOWBALL")
+EGG              = Entity(82, "EGG")
+
+# Item tiles
+for name in block.__dir__():
+    if name.isupper():
+        tile = getattr(block, name)
+        globals()[f'ITEM_{name}'] = Item(tile.id, name)
+
+ITEM = ITEM_STONE
+
+# Setup variables
+del block, name, tile
